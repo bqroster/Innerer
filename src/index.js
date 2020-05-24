d224bf6bc17d0ef527153ad541a8cd06ee86ef9d
joseburgos <=>
2020-05-24 21:01:16 +0200

    Add DOMnextick for testing with JSFrameworks routers

--- src/index.js
+++ src/index.js
@@ -79,23 +79,6 @@ export const ABOVE_CENTER = 'above_center';
 export const EMMIT_FN_TEST = '__test_FN__';
 
 /**
- * check DOM is loaded and execute fn()
- * @param {function} fn
- */
-const DOMReady = function(fn) {
-    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
-        fn();
-    } else {
-        if (document.addEventListener) {
-            document.addEventListener("DOMContentLoaded", fn);
-        }
-        else {
-            window.onload = fn;
-        }
-    }
-};
-
-/**
  * @return {number}
  */
 const getBodyScrollY = function() {
@@ -316,57 +299,36 @@ const viewportUpdatedHandler = function() {
     });
 };
 
+const resetValues = function() {
+    DOMRef = null;
+    isMounted = 'none';
+    dataInners = null;
+};
+
 /**
  * @return {object}
  */
 const getEmptyEmmitDataObj = function() {
     return {
         tag: EMMIT_FN_TEST,
-        top: 0,
+        top: 9999,
         right: 0,
-        bottom: 0,
+        bottom: 9999,
         left: 0,
         width: 0,
         height: 0,
         direction: DIRECTION_ST,
         position: {
             status: BOTTOM_OUTER,
-            percentageInTransition: 0,
-            percentageInPosition: 0,
-            percentageOutside: 1
+            percentageInTransition: 9999,
+            percentageInPosition: 9999,
+            percentageOutside: 9999
         },
         centered: {
             status: BELOW_CENTER,
-            percentage: 0
+            percentage: 9999
         },
     };
-};
-
-const DOMLoaded = function(ev) {
-
-    if (!((ev.target.readyState === 'complete' || ev.target.readyState !== 'loading') && isMounted === 'none')) return;
-
-    isMounted = 'progress';
-
-    const ref = DOMRef ? document.querySelector(DOMRef) : document;
-    if (ref === null) throw Error(`${ERROR_TAG}: Reference ${DOMRef} not found on DOM.`);
-
-    // ac read all the attributes `innerer`
-    dataInners = ref.querySelectorAll(`[${QUERY_ATTR}]`);
-
-    if (dataInners.length === 0) {
-        console.warn(`${ERROR_TAG}: There is no elements to track in current document.`);
-        return;
-    }
-
-    // ac get current scroll Y
-    bodyScrollY = getBodyScrollY();
-
-    // ac set listeners for scroll and resize
-    window.addEventListener('scroll', viewportUpdatedHandler);
-    window.addEventListener('resize', viewportUpdatedHandler);
-
-    isMounted = 'completed';
 };
 
 /**
@@ -403,6 +365,78 @@ const processRefHandler = function(ref = null) {
         throw Error(`${ERROR_TAG}: Reference set invalid format, expected string, got ${typeof ref}`);
 };
 
+const DOMLoaded = function(ev) {
+
+    if (!((document.readyState === 'complete' || document.readyState !== 'loading'))) return;
+
+    isMounted = 'progress';
+
+    /**
+     * give some time to
+     * JS-Frameworks to load page component
+     * on the router-view
+     * Mostly if user installed the createrInners
+     * in a lifecycle method before mounting the
+     * template on the DOM (or virtual-dom)
+     *
+     * @param {number} counter
+     */
+    const processDOMQuerySelector = function(counter = 0) {
+
+        if (counter > 25) throw Error(`${ERROR_TAG}: Reference ${DOMRef} not found on DOM.`);
+
+        const ref = DOMRef ? document.querySelector(DOMRef) : document;
+
+        if (ref === null) {
+            setTimeout(
+                processDOMQuerySelector.bind(this, counter+1),
+                50
+            );
+        }
+        else {
+            DOMProcess(ref);
+        }
+    };
+
+    processDOMQuerySelector();
+};
+
+const DOMProcess = function(ref) {
+    // ac read all the attributes `innerer`
+    dataInners = ref.querySelectorAll(`[${QUERY_ATTR}]`);
+
+    if (dataInners.length === 0) {
+        console.warn(`${ERROR_TAG}: There is no elements to track in current document.`);
+        return;
+    }
+
+    // ac get current scroll Y
+    bodyScrollY = getBodyScrollY();
+
+    // ac set listeners for scroll and resize
+    window.addEventListener('scroll', viewportUpdatedHandler);
+    window.addEventListener('resize', viewportUpdatedHandler);
+
+    isMounted = 'completed';
+};
+
+/**
+ * check DOM is loaded and execute fn()
+ * @param {function} fn
+ */
+const DOMReady = function(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    } else {
+        if (document.addEventListener) {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+        else {
+            window.onload = fn;
+        }
+    }
+};
+
 /**
  * @param {function} data
  * @param {string|null} ref
@@ -420,9 +454,11 @@ export const createInners = function(data, ref = null) {
 };
 
 export const destroyInners = function() {
-    if (dataInners === undefined || dataInners === null || dataInners.length === 0) return;
-    window.removeEventListener('scroll', viewportUpdatedHandler);
-    window.removeEventListener('resize', viewportUpdatedHandler);
+    // if (dataInners === undefined || dataInners === null || dataInners.length === 0) return;
+    // resetValues();
+    console.log('reseting values');
+    // window.removeEventListener('scroll', viewportUpdatedHandler);
+    // window.removeEventListener('resize', viewportUpdatedHandler);
 };
 
 export default {
